a0777aecea7a6ad37dca73c3f96caf2457d48136
juliana <julianapereira.dev@gmail.com>
2023-07-13 14:25:20 -0300

    Feat: Vínculo entre produto e a rota do carrinho (incompleto)

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {schemaValidation} from "../middlewares/schemaValidation.js"
-import { getProduct, postProduct, getProducts } from "../controllers/products.controllers.js";
+import { getProduct, postProduct, getProducts, addToCart } from "../controllers/products.controllers.js";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
 import { postProductSchema } from "../schemas/products.schemas.js";
 
@@ -10,5 +10,6 @@ const productRouter = Router()
 productRouter.get("/home", tokenValidation, getProducts)
 productRouter.get("/product/:id", tokenValidation, getProduct)
 productRouter.post("/product", schemaValidation(postProductSchema), postProduct)
+productRouter.post("/product/:id/update-shopping-cart", tokenValidation, addToCart);
 
 export default productRouter
