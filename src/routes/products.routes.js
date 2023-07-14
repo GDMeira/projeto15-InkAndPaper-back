413ad79b8c352df7a9258bb4989cd467b66b7869
GDMeira <gabrieldega970@gmail.com>
2023-07-14 19:11:49 -0300

    refactor: retirada do endpoint de adicionar produto ao carrinho pela products.routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,7 +3,6 @@ import {schemaValidation} from "../middlewares/schemaValidation.js"
 import { getProduct, postProduct, getProducts } from "../controllers/products.controllers.js";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
 import { postProductSchema } from "../schemas/products.schemas.js";
-import { addToCart } from "../controllers/cart.controllers.js";
 
 
 const productRouter = Router()
@@ -11,6 +10,5 @@ const productRouter = Router()
 productRouter.get("/home", tokenValidation, getProducts)
 productRouter.get("/product/:id", tokenValidation, getProduct)
 productRouter.post("/product", schemaValidation(postProductSchema), postProduct)
-productRouter.post("/product/:id/update-shopping-cart", tokenValidation, addToCart);
 
 export default productRouter
