3dd80cc9c2135649b6849efab304bab0e9e4021d
GDMeira <gabrieldega970@gmail.com>
2023-07-15 01:44:50 -0300

    feature: criado o endpoint para login com google

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import {schemaValidation} from "../middlewares/schemaValidation.js"
 import { signInSchema, signUpSchema } from "../schemas/auth.schemas.js";
-import { logout, signIn, signUp } from "../controllers/auth.controllers.js";
+import { logout, signIn, signInByGoogle, signUp } from "../controllers/auth.controllers.js";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
 
 const authRouter = Router()
 
 authRouter.post("/sign-up", schemaValidation(signUpSchema), signUp)
 authRouter.post("/sign-in", schemaValidation(signInSchema), signIn)
+authRouter.post('/sign-in/google', signInByGoogle)
 authRouter.delete('/logout', tokenValidation, logout)
 
 export default authRouter
