6ba7c394e00108175c985960f07c0e18a3bf2116
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-16 23:30:29 -0300

    Feat: myOrders

--- src/routes/checkout.routes.js
+++ src/routes/checkout.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
-import { postCheckOut, getCheckoutItems } from "../controllers/checkout.controllers.js"
+import { postCheckOut, getCheckoutItems, getCheckoutLastItems } from "../controllers/checkout.controllers.js"
 
 const checkoutRouter = Router();
 
 checkoutRouter.post("/payment", tokenValidation, postCheckOut);
-checkoutRouter.get("/checkout", tokenValidation, getCheckoutItems );
+checkoutRouter.get("/checkout", tokenValidation, getCheckoutLastItems );
+checkoutRouter.get("/myorders", tokenValidation, getCheckoutItems );
 
 export default checkoutRouter;
